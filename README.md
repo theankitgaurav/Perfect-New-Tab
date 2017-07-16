e2cfa1df0b693c054f5af9c3feeafbfe0a62ebad
Ankit Gaurav <ankitgaurav@outlook.com>
2017-07-16 13:38:47 +0530

    Restructure files, take care of empty lists

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
 # Perfect-New-Tab :fire:
-A Google Chrome extension that replaces the existing new tab with a more useful and feature-rich one.
+A Google Chrome extension that replaces the existing new tab with a more useful and feature-rich one. The extension can be installed for free from [here](https://goo.gl/HhJTdk)
 
 # Screenshot :camera:
 ![Perfect New Tab Screenshot](https://lh3.googleusercontent.com/3geJd4fakGaK0hSNsDGMlWvCdrPkNlv5vGJCBSWBGbQYE23EFV6N6tDbmuZejhX0xrZrbP97Nw=s640-h400-e365-rw)
 
-# Install :arrow_double_down:
-The extension can be installed for free from [here](https://goo.gl/HhJTdk) 
+# Features :wink:
++ Useful shortcuts in the dock (Like that of Firefox)
++ Visit the Most frequent sites with ease
++ A built-in simple Todos manager 
 
 # Contributions :heart_eyes:
 Comments and improvements are more than welcome!
---
+++ app/js/main.js
@@ -0,0 +1,188 @@
+chrome.contextMenus.removeAll()
+//This method opens all links overridding chrome blockage of file-system urls
+function openLink(url) {
+    if (url === 'restore session') {
+        chrome.sessions.restore();
+        return;
+    }
+    chrome.tabs.update({ 'url': url, 'selected': true })
+}
+
+// Frequent Sites logic
+var f_sites = new Vue({
+    el: '#f_sites',
+    data: {
+        frequentSitesArray: [],
+        numberOfItems: 10
+    },
+    created: function() {
+        this.getFrequentSites();
+    },
+    watch: {
+        numberOfItems: function() {
+            this.getFrequentSites();
+        }
+    },
+    methods: {
+        getFrequentSites: function() {
+            var self = this;
+            chrome.topSites.get((result) => {
+                self.numberOfItems = (self.numberOfItems < result.length) ? self.numberOfItems: result.length
+                self.frequentSitesArray = []
+                for (var i = 0; i < self.numberOfItems; i++) {
+                    self.frequentSitesArray.push({
+                        'title': result[i].title,
+                        'url': result[i].url,
+                        'faviconUrl': 'chrome://favicon/' + result[i].url
+                    })
+                }
+            })
+        },
+        openLink: openLink
+    }
+})
+
+// Recent Bookmarks logic
+var r_bookmarks = new Vue({
+    el: '#r_bookmarks',
+    data: {
+        recentBookmarksArray: [],
+        numberOfItems: 10,
+        checkedItems: [],
+        deleteMode: false
+    },
+    created: function() {
+        this.getRecentBookmarks()
+    },
+    watch: {
+        numberOfItems: function() {
+            this.getRecentBookmarks()
+        }
+    },
+    computed: {
+      focusDeleteBtn: function(){
+        return !this.deleteMode
+      }
+    },
+    methods: {
+        deleteCheckedBookmarks: function() {
+          if(this.deleteMode === false){
+            this.deleteMode = true
+          } else {
+            this.checkedItems.forEach(function(elem){
+              chrome.bookmarks.remove(elem)
+            })
+            this.checkedItems = []
+            this.getRecentBookmarks()
+            this.deleteMode = false
+          }
+        },
+        getRecentBookmarks: function() {
+            const self = this
+            self.recentBookmarksArray = []
+            const positiveLength = 1 // The first parameter can't be less than 1
+            self.numberOfItems = self.numberOfItems || positiveLength
+            chrome.bookmarks.getRecent(self.numberOfItems, (result) => {
+                self.numberOfItems = (self.numberOfItems < result.length)?self.numberOfItems: result.length
+                for (var i = 0; i < self.numberOfItems; i++) {
+                    self.recentBookmarksArray.push({
+                        'title': result[i].title,
+                        'url': result[i].url,
+                        'faviconUrl': 'chrome://favicon/' + result[i].url,
+                        'id': result[i].id
+                    })
+                }
+            })
+        },
+        loadMore: function(){
+            self.numberOfItems += 10
+        },
+        openLink: openLink
+    }
+})
+
+// ToDo list logic
+var todoStorage = {
+    save: function(todos) {
+        chrome.storage.sync.set({ 'perfect_new_tab_todos': todos }, function() {
+            if (chrome.runtime.lastError) {
+                throw ('Runtime lastError while saving data to chrome storage')
+            }
+        })
+    }
+}
+var todos_app = new Vue({
+    el: '#todos_app',
+    data: {
+        app_name: 'Todos',
+        todos: [],
+        todoText: ''
+    },
+    watch: {
+        todos: {
+            deep: true,
+            handler: function(todos) {
+                todoStorage.save(todos)
+            }
+        }
+    },
+    computed: {
+        pendingTodos: function() {
+            return this.todos.filter(function(el) {
+                return !el.done;
+            })
+        },
+        doneItems: function() {
+            return this.todos.filter(function(el) {
+                return el.done
+            })
+        }
+    },
+    created: function() {
+        var self = this
+        chrome.storage.sync.get('perfect_new_tab_todos', function(items) {
+            if (!chrome.runtime.lastError) {
+                if (items.perfect_new_tab_todos != null) {
+                    self.todos = items.perfect_new_tab_todos
+                } else {
+                    console.log('No todo items in Chrome Storage')
+                }
+            } else {
+                console.error("Runtime Error while fetching data from Chrome Storage")
+            }
+        })
+    },
+    methods: {
+        addTodo: function(todoText) {
+            var text = this.todoText && this.todoText.trim()
+            if (text == '') {
+                this.todoText = ''
+                return
+            }
+            var todo = {
+                'todoId': todoStorage.uid++,
+                'text': text,
+                'done': false,
+                'timeAdded': Date.now()
+            }
+            this.todos.push(todo)
+            this.todoText = ''
+        },
+        checkItems: function() {
+            document.getElementById('trash').style.visibility = 'visible'
+        },
+        deleteItems: function(){
+            this.todos = this.todos.filter(function(el){
+                return el.done === false
+            })
+        }
+    }
+})
+
+// Footer logic
+var footer = new Vue({
+    el: '#footer',
+    methods: {
+        openLink: openLink
+    }
+})
